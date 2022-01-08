30941cf5fd88e6efae6f89b6e088c0b8e9a3585b
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2022-01-08 14:40:06 -0700

    updated functionality

--- server.js
+++ server.js
@@ -121,12 +121,25 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+
 // ('/') - this is the root route of the server which is used to create a homepage for the server
 // below has one job - to send our user the html page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
-})
+});
